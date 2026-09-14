13fcb5adce0b24626ffa90aef38a3cb312d9a073
Aditya Mishra <adityamishra19880@gmail.com>
2026-09-15 01:00:26 +0530

    feat(course): add Course Structure component with drag-and-drop functionality

---
+++ app/admin/courses/[courseId]/edit/_components/CourseStructure.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import {
+  DndContext,
+  rectIntersection,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  sortableKeyboardCoordinates,
+  arrayMove,
+} from "@dnd-kit/sortable";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export function CourseStructure() {
+  const [items, setItems] = useState([1, 2, 3]);
+
+  function SortableItem(props) {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+      useSortable({ id: props.id });
+
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+    return (
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        {props.id}
+      </div>
+    );
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  return (
+    <DndContext
+      collisionDetection={rectIntersection}
+      onDragEnd={handleDragEnd}
+      sensors={sensors}
+    >
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between border-b border-border">
+          <CardTitle>Chapters</CardTitle>
+          {/* <NewChapterModal courseId={data.id} /> */}
+        </CardHeader>
+
+        <CardContent>
+          <SortableContext strategy={verticalListSortingStrategy} items={items}>
+            {items.map((id) => (
+              <SortableItem key={id} id={id} />
+            ))}
+          </SortableContext>
+        </CardContent>
+      </Card>
+    </DndContext>
+  );
+}
--- app/admin/courses/[courseId]/edit/page.tsx
+++ app/admin/courses/[courseId]/edit/page.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EditCourseForm } from "./_components/EditCourseForm";
+import { CourseStructure } from "./_components/CourseStructure";
 
 type Params = Promise<{ courseId: string }>;
 
@@ -15,7 +16,6 @@ export default async function EditRoute({ params }: { params: Params }) {
   const { courseId } = await params;
   const data = await adminGetCourse(courseId);
 
-  console.log("data: ", data)
   return (
     <div>
       <h1 className="text-3xl font-bold mb-8">
@@ -23,7 +23,7 @@ export default async function EditRoute({ params }: { params: Params }) {
         <span className="text-primary underline">{data.title}</span>
       </h1>
 
-      <Tabs defaultValue="basic-info" className="w-full">
+      <Tabs defaultValue="course-structure" className="w-full">
         <TabsList className="grid grid-cols-2 w-full">
           <TabsTrigger
             value="basic-info"
@@ -51,6 +51,20 @@ export default async function EditRoute({ params }: { params: Params }) {
             </CardHeader>
             <CardContent>
               <EditCourseForm data={data} />
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="course-structure">
+          <Card>
+            <CardHeader>
+              <CardTitle>Course Structure</CardTitle>
+              <CardDescription>
+                Here you can update your Course Structure
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <CourseStructure />
             </CardContent>
           </Card>
         </TabsContent>
